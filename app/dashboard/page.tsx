81fcbd6bcb3d29d8ebe7eafa40cd14814e1bcbc2
Dhanya MD <dhanya13md@gmail.com>
2024-10-23 00:33:16 +0530

    event types

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,45 @@
+import { EmptyState } from "../components/EmptyState";
+import prisma from "../lib/db";
+import { getUser } from "../lib/hooks";
 
-import { getUser } from "../lib/hooks.ts";
-
+async function getData( id : string){
+  const data = await prisma.user.findUnique({
+    where : {
+      id : id
+    },
+    select : {
+      userName : true,
+      eventType : {
+        select : {
+          id : true,
+          active : true,
+          url : true,
+          title : true,
+          duration : true
+        }
+      }
+    }
+  })
+  if(!data){
+    return null
+  }
+  return data;
+}
 export default async function DashboardPage() {
   const session = await getUser();
+  const data = await getData(session?.user?.id as string)
   return (
-    <div>
-
-    </div>
+    <>
+    {data?.eventType.length === 0 ? (
+      <EmptyState 
+      title="You have no event types"
+      description="You can create your first evnt by clicking the button below"
+      buttonText="Add event type"
+      href="/dashboard/new"
+      />
+    ) : (
+      <p></p>
+    )}
+    </>
   )
 }
